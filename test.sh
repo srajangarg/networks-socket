dac1a447b50f70598c1b84e75639d163740b4cf8
Srajan Garg <srajan.garg@gmail.com>
2016-04-03 18:27:14 +0530

    added test script

---
+++ test.sh
@@ -0,0 +1,38 @@
+echo "Pass 6"
+
+./user 10.3.32.56 5555 ak/PCENVe1pBs 6 001
+./user 10.3.32.56 5555 wa.fWXDts2As2 6 001
+./user 10.3.32.56 5555 noMcOFsjFYDh6 6 001
+./user 10.3.32.56 5555 nkucayW/QpIRM 6 001
+./user 10.3.32.56 5555 czrrj5Yct9W6M 6 001
+./user 10.3.32.56 5555 qyoZ7XoNFGEd2 6 001
+./user 10.3.32.56 5555 lq4fZ8zH48BXw 6 001
+./user 10.3.32.56 5555 ga3A8ym4k7w0A 6 001
+./user 10.3.32.56 5555 qa9KyxGM.JB7k 6 001
+./user 10.3.32.56 5555 hen2lPsyU/XYI 6 001
+
+echo "Pass 7"
+
+./user 10.3.32.56 5555 ojLYhx3Wk7czA 7 001
+./user 10.3.32.56 5555 ihr7ko/isTYF6 7 001
+./user 10.3.32.56 5555 yhV5DiKIlCL.A 7 001
+./user 10.3.32.56 5555 liKAio78F3DWc 7 001
+./user 10.3.32.56 5555 kx7Kw0Q8KKLPk 7 001
+./user 10.3.32.56 5555 hsOVrVrouIypk 7 001
+./user 10.3.32.56 5555 qstOaJOApvbZI 7 001
+./user 10.3.32.56 5555 ei8bWwWkVogGY 7 001
+./user 10.3.32.56 5555 ovnqxU2OCqpyU 7 001
+./user 10.3.32.56 5555 ccRSXytW3c5nQ 7 001
+
+echo "Pass 8"
+
+./user 10.3.32.56 5555 jovm/.63adpfM 8 001
+./user 10.3.32.56 5555 bx8eFMNsWg8is 8 001
+./user 10.3.32.56 5555 adRTcN1xrP3Vo 8 001
+./user 10.3.32.56 5555 zl8tSE9pYOJgs 8 001
+./user 10.3.32.56 5555 tmjmfdlS6BC.M 8 001
+./user 10.3.32.56 5555 gnDc.cmfR5JOo 8 001
+./user 10.3.32.56 5555 teBilQAC5sLLM 8 001
+./user 10.3.32.56 5555 zblQ2g9fUunfU 8 001
+./user 10.3.32.56 5555 moIoKLiusYvbw 8 001
+./user 10.3.32.56 5555 bg9baBkZoc6o2 8 001
